0c8c50d3da1a4547de136fe998f97fb61fa2dbce
anh <191428+anh@users.noreply.github.com>
2021-01-19 14:13:37 +0700

    fix missing on_delete

--- cmsplugin_feedparser/migrations/0001_initial.py
+++ cmsplugin_feedparser/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FeedparserPluginModel',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE,)),
                 ('url', models.CharField(max_length=255, verbose_name='url')),
                 ('renderer', models.CharField(default=b'basic-xml', help_text='Feed renderer, your feed must be compatible with it', max_length=100, verbose_name='renderer', choices=[(b'basic-json', b'basic-json'), (b'basic-xml', b'basic-xml')])),
                 ('template', models.CharField(help_text='Template used to render the feed', max_length=100, verbose_name='template', choices=[(b'django_feedparser/basic_feed_renderer.html', b'Default template')])),
